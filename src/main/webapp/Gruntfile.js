d9239dd34a44f5499960287e9fcce196a2bd050e
윤승환 <shyoon05@gmail.com>
2016-12-26 19:14:40 +0900

    핸들바, grunt 기반

---
+++ src/main/webapp/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        'handlebars': {
+            options: {
+                namespace: "Handlebars.templates",
+                processName:function(filePath) {
+                    var pattern=/handlebar\/(.+\/)*(.+)\.hbs/gi;
+                    return pattern.exec(filePath)[2];
+                }
+            },
+            compile : {
+                files: {
+                    //destination : [target list]
+                    "templates.js" : ["handlebar/*.hbs"]
+                }
+            }
+        },
+        // grunt watch 명령으로 아래 옵션을 실행
+        watch:{
+            files:'handlebar/*.hbs',
+            tasks: ['default','handlebars']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['handlebars']); //grunt 명령어로 실행할 작업
+
+};
